bcebde0dc6d005cf0766f88c6b6d31a6c4d5de1e
omkathalkar <kathalkarom47@gmail.com>
2026-09-24 23:11:49 +0000

    Deploying to gh-pages from @ omkathalkar/omkathalkar.github.io@f0356c668584e5f26a3df7aafed584ea77c385cf 🚀

---
+++ makanmate-owner/sw.js
@@ -0,0 +1,1 @@
+const C="mm-owner-v1",F=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(e=>e.addAll(F))),self.skipWaiting()}),self.addEventListener("activate",e=>e.waitUntil(self.clients.claim())),self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request))));
